799f1d02f7c8020f80bacda1493918f3a396349f
Mattia Piccinetti <mattia.piccinetti@jobrapido.com>
2016-05-28 16:19:43 +0200

    pascal triangle exercise is done

--- functional-programming-principles-in-scala/recfun/src/main/scala/recfun/Main.scala
+++ functional-programming-principles-in-scala/recfun/src/main/scala/recfun/Main.scala
@@ -11,17 +11,19 @@ object Main {
   }
 
   /**
-   * Exercise 1
-   */
-    def pascal(c: Int, r: Int): Int = ???
+    * Exercise 1
+    */
+  def pascal(c: Int, r: Int): Int =
+    if (c == 0 || c == r) 1
+    else pascal(c - 1, r - 1) + pascal(c, r - 1)
   
   /**
-   * Exercise 2
-   */
-    def balance(chars: List[Char]): Boolean = ???
-  
+    * Exercise 2
+    */
+  def balance(chars: List[Char]): Boolean = ???
+
   /**
-   * Exercise 3
-   */
-    def countChange(money: Int, coins: List[Int]): Int = ???
-  }
+    * Exercise 3
+    */
+  def countChange(money: Int, coins: List[Int]): Int = ???
+}
