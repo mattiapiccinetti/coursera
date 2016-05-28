ce8a72d8caa36bb69954d54393422a1552ebbec2
Mattia Piccinetti <mattiapiccinetti@users.noreply.github.com>
2016-05-28 17:46:17 +0200

    parentheses balancing exercise is done

--- functional-programming-principles-in-scala/recfun/src/main/scala/recfun/Main.scala
+++ functional-programming-principles-in-scala/recfun/src/main/scala/recfun/Main.scala
@@ -16,11 +16,30 @@ object Main {
   def pascal(c: Int, r: Int): Int =
     if (c == 0 || c == r) 1
     else pascal(c - 1, r - 1) + pascal(c, r - 1)
-  
+
   /**
     * Exercise 2
     */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+    def evaluate(char: Char): Int = {
+      char match {
+        case '(' => +1
+        case ')' => -1
+        case _ =>  0
+      }
+    }
+
+    def loop(acc: Int, chars: List[Char]): Boolean = {
+      chars match {
+        case _ if acc < 0 => false
+        case Nil if acc > 0 => false
+        case Nil if acc == 0 => true
+        case _ => loop(acc + evaluate(chars.head), chars.tail)
+      }
+    }
+
+    loop(0, chars)
+  }
 
   /**
     * Exercise 3
