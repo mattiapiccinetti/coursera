89ad8ce8be4754643867173df008fb30b448fd58
Mattia Piccinetti <mattia.piccinetti@jobrapido.com>
2016-06-12 01:08:41 +0200

    GoogleVsApple implementation is complete

--- functional-programming-principles-in-scala/objsets/src/main/scala/objsets/TweetSet.scala
+++ functional-programming-principles-in-scala/objsets/src/main/scala/objsets/TweetSet.scala
@@ -201,15 +201,15 @@ object GoogleVsApple {
   val google = List("android", "Android", "galaxy", "Galaxy", "nexus", "Nexus")
   val apple = List("ios", "iOS", "iphone", "iPhone", "ipad", "iPad")
 
-  lazy val googleTweets: TweetSet = ???
-  lazy val appleTweets: TweetSet = ???
+  lazy val googleTweets: TweetSet = TweetReader.allTweets.filter(tweet => google.exists(tweet.text.contains _))
+  lazy val appleTweets: TweetSet = TweetReader.allTweets.filter(tweet => apple.exists(tweet.text.contains _))
 
   /**
    * A list of all tweets mentioning a keyword from either apple or google,
    * sorted by the number of retweets.
    */
-     lazy val trending: TweetList = ???
-  }
+  lazy val trending: TweetList = (googleTweets union appleTweets) descendingByRetweet
+}
 
 object Main extends App {
   // Print the trending tweets
