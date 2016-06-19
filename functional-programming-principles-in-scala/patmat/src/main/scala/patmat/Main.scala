00758314a6cf62744e0de954c566757581874e65
Mattia Piccinetti <mattia.piccinetti@jobrapido.com>
2016-06-19 17:16:20 +0200

    implemented encode and decode functions, until functions is under test

---
+++ functional-programming-principles-in-scala/patmat/src/main/scala/patmat/Main.scala
@@ -0,0 +1,9 @@
+package patmat
+
+import patmat.Huffman._
+
+object Main {
+  def main(args: Array[String]) {
+    println(decodedSecret.mkString)
+  }
+}
--- functional-programming-principles-in-scala/patmat/src/test/scala/patmat/HuffmanSuite.scala
+++ functional-programming-principles-in-scala/patmat/src/test/scala/patmat/HuffmanSuite.scala
@@ -1,4 +1,4 @@
-  package patmat
+package patmat
 
 import org.scalatest.FunSuite
 
@@ -14,7 +14,6 @@ class HuffmanSuite extends FunSuite {
     val t2 = Fork(Fork(Leaf('a',2), Leaf('b',3), List('a','b'), 5), Leaf('d',4), List('a','b','d'), 9)
   }
 
-
   test("weight of a larger tree") {
     new TestTrees {
       assert(weight(t1) === 5)
@@ -24,12 +23,13 @@ class HuffmanSuite extends FunSuite {
 
   test("frequency of each character in the text") {
     new TestTrees {
-      assert(times(List[Char]()) === Nil)
-      assert(times(List('x')) === List(('x', 1)))
-      assert(times(List('x', 'x')) === List(('x', 2)))
-      assert(times(List('f', 'o', 'o')) === List(('f', 1), ('o', 2)))
-      assert(times(List('b', 'a', 'r')) === List(('b', 1), ('a', 1), ('r', 1)))
-      assert(times(List('b', 'o', 'b')) === List(('b', 2), ('o', 1)))
+      assert(times("".toList) === List())
+      assert(times("x".toList) === List(('x', 1)))
+      assert(times("xx".toList) === List(('x', 2)))
+      assert(times("foo".toList) === List(('f', 1), ('o', 2)))
+      assert(times("bar".toList) === List(('b', 1), ('a', 1), ('r', 1)))
+      assert(times("bob".toList) === List(('b', 2), ('o', 1)))
+      assert(times("puzzle bubble".toList) === List(('p', 1), ('u', 2), ('z', 2), ('l', 2), ('e', 2), (' ', 1), ('b', 3)))
     }
   }
 
@@ -39,11 +39,9 @@ class HuffmanSuite extends FunSuite {
     }
   }
 
-
   test("string2chars(\"hello, world\")") {
     assert(string2Chars("hello, world") === List('h', 'e', 'l', 'l', 'o', ',', ' ', 'w', 'o', 'r', 'l', 'd'))
   }
-
 
   test("makeOrderedLeafList for some frequency table") {
     assert(makeOrderedLeafList(List(('t', 2), ('e', 1), ('x', 3))) === List(Leaf('e',1), Leaf('t',2), Leaf('x',3)))
@@ -65,20 +63,31 @@ class HuffmanSuite extends FunSuite {
     assert(combine(leaflist) === List(Fork(Leaf('e', 1), Leaf('t', 2), List('e', 't'), 3), Leaf('x', 4)))
   }
 
-  test("create a code tree from a list of char") {
+  test("until") {
+    val actual = until(singleton, combine)(List(Leaf('e', 1), Leaf('t', 2), Leaf('x', 4)))
+    val expected = List(Fork(Fork(Leaf('e', 1), Leaf('t', 2), List('e', 't'), 3), Leaf('x', 4), List('e', 't', 'x'), 7))
+
+    assert(actual === expected)
+  }
+
+  test("create a simple code tree from a list of char") {
     assert(createCodeTree("foo".toList) === Fork(Leaf('f', 1), Leaf('o', 2), List('f', 'o'), 3))
   }
 
   test("decode the bit sequence into a list of chars") {
     val tree = Fork(Leaf('f', 1), Leaf('o', 2), List('f', 'o'), 3)
-    val bits = List(1)
+    val bits = List(0, 1, 1)
 
-    assert(decode(tree, bits) === List('f', 'o', 'o'))
+    assert(decode(tree, bits).mkString === "foo")
   }
 
   test("decode and encode a very short text should be identity") {
     new TestTrees {
       assert(decode(t1, encode(t1)("ab".toList)) === "ab".toList)
     }
+  }
+
+  test("encode the text into a list of bits") {
+    assert(encode(Fork(Leaf('f', 1), Leaf('o', 2), List('f', 'o'), 3))("foo".toList) === List(0, 1, 1))
   }
 }
