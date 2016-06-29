8d4a65efc397b45876d213035ec1c1a66853b634
Mattia Piccinetti <mattiapiccinetti@users.noreply.github.com>
2016-06-29 23:35:45 +0200

    wordAnagrams done

--- functional-programming-principles-in-scala/forcomp/src/main/scala/forcomp/Anagrams.scala
+++ functional-programming-principles-in-scala/forcomp/src/main/scala/forcomp/Anagrams.scala
@@ -58,7 +58,7 @@ object Anagrams {
   lazy val dictionaryByOccurrences: Map[Occurrences, List[Word]] = dictionary groupBy wordOccurrences _
 
   /** Returns all the anagrams of a given word. */
-  def wordAnagrams(word: Word): List[Word] = ???
+  def wordAnagrams(word: Word): List[Word] = dictionaryByOccurrences(wordOccurrences(word))
 
   /** Returns the list of all subsets of the occurrence list.
    *  This includes the occurrence itself, i.e. `List(('k', 1), ('o', 1))`
