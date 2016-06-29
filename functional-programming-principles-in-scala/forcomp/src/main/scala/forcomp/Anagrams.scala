d78e0754c771e93a042357da6143ddbb95fcabab
Mattia Piccinetti <mattia.piccinetti@jobrapido.com>
2016-06-29 22:49:48 +0200

    wordOccurrences done

--- functional-programming-principles-in-scala/forcomp/src/main/scala/forcomp/Anagrams.scala
+++ functional-programming-principles-in-scala/forcomp/src/main/scala/forcomp/Anagrams.scala
@@ -34,7 +34,8 @@ object Anagrams {
    *
    *  Note: you must use `groupBy` to implement this method!
    */
-  def wordOccurrences(w: Word): Occurrences = ???
+  def wordOccurrences(w: Word): Occurrences =
+    w.toLowerCase.groupBy(x => x).map { case (k, v) => (k, v.size) }.toList.sorted
 
   /** Converts a sentence into its character occurrence list. */
   def sentenceOccurrences(s: Sentence): Occurrences = ???
