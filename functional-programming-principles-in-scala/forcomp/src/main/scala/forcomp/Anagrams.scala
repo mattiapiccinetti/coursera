034197be82a69a64e23fe6ac4c2ccfe4f30d990d
Mattia Piccinetti <mattia.piccinetti@jobrapido.com>
2016-07-01 01:07:33 +0200

    combination done but very very unreadable #shameonme

--- functional-programming-principles-in-scala/forcomp/src/main/scala/forcomp/Anagrams.scala
+++ functional-programming-principles-in-scala/forcomp/src/main/scala/forcomp/Anagrams.scala
@@ -82,7 +82,20 @@ object Anagrams {
    *  Note that the order of the occurrence list subsets does not matter -- the subsets
    *  in the example above could have been displayed in some other order.
    */
-  def combinations(occurrences: Occurrences): List[Occurrences] = ???
+
+  def combinations(occurrences: Occurrences): List[Occurrences] = {
+    def isValidCombination(combo: List[(Char, Int)]) = {
+      val c = combo map { _._1 }
+      c.distinct.size == c.size
+    }
+
+    occurrences.flatMap { case (c, n) => (1 to n) map { (c, _) } }
+      .toSet
+      .subsets
+      .map { _ toList }
+      .filter { x => x.size <= occurrences.size && isValidCombination(x) }
+      .toList
+  }
 
   /** Subtracts occurrence list `y` from occurrence list `x`.
    *
