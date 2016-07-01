6de32ff9b84f7bca807f9a7f3419bfe09e153d5b
Mattia Piccinetti <mattia.piccinetti@jobrapido.com>
2016-07-01 19:12:27 +0200

    sentenceAnagrams is done #coulBeBetterThanThis

--- functional-programming-principles-in-scala/forcomp/src/test/scala/forcomp/AnagramsSuite.scala
+++ functional-programming-principles-in-scala/forcomp/src/test/scala/forcomp/AnagramsSuite.scala
@@ -129,31 +129,31 @@ class AnagramsSuite extends FunSuite  {
     val sentence = List()
     assert(sentenceAnagrams(sentence) === List(Nil))
   }
-  
-  // test("sentence anagrams: Linux rulez") {
-  //   val sentence = List("Linux", "rulez")
-  //   val anas = List(
-  //     List("Rex", "Lin", "Zulu"),
-  //     List("nil", "Zulu", "Rex"),
-  //     List("Rex", "nil", "Zulu"),
-  //     List("Zulu", "Rex", "Lin"),
-  //     List("null", "Uzi", "Rex"),
-  //     List("Rex", "Zulu", "Lin"),
-  //     List("Uzi", "null", "Rex"),
-  //     List("Rex", "null", "Uzi"),
-  //     List("null", "Rex", "Uzi"),
-  //     List("Lin", "Rex", "Zulu"),
-  //     List("nil", "Rex", "Zulu"),
-  //     List("Rex", "Uzi", "null"),
-  //     List("Rex", "Zulu", "nil"),
-  //     List("Zulu", "Rex", "nil"),
-  //     List("Zulu", "Lin", "Rex"),
-  //     List("Lin", "Zulu", "Rex"),
-  //     List("Uzi", "Rex", "null"),
-  //     List("Zulu", "nil", "Rex"),
-  //     List("rulez", "Linux"),
-  //     List("Linux", "rulez")
-  //   )
-  //   assert(sentenceAnagrams(sentence).toSet === anas.toSet)
-  // }
+
+  test("sentence anagrams: Linux rulez") {
+    val sentence = List("Linux", "rulez")
+    val anas = List(
+      List("Rex", "Lin", "Zulu"),
+      List("nil", "Zulu", "Rex"),
+      List("Rex", "nil", "Zulu"),
+      List("Zulu", "Rex", "Lin"),
+      List("null", "Uzi", "Rex"),
+      List("Rex", "Zulu", "Lin"),
+      List("Uzi", "null", "Rex"),
+      List("Rex", "null", "Uzi"),
+      List("null", "Rex", "Uzi"),
+      List("Lin", "Rex", "Zulu"),
+      List("nil", "Rex", "Zulu"),
+      List("Rex", "Uzi", "null"),
+      List("Rex", "Zulu", "nil"),
+      List("Zulu", "Rex", "nil"),
+      List("Zulu", "Lin", "Rex"),
+      List("Lin", "Zulu", "Rex"),
+      List("Uzi", "Rex", "null"),
+      List("Zulu", "nil", "Rex"),
+      List("rulez", "Linux"),
+      List("Linux", "rulez")
+    )
+    assert(sentenceAnagrams(sentence).toSet === anas.toSet)
+  }
 }
